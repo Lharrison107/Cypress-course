c404febb3676926263ea6e3faee558f864853cee
Lyric Harrison <lharrison@diesellaptops.com>
2022-05-16 16:44:29 -0400

    part 9 section 2

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,47 @@
+
+
+
+describe('Our first suite', () => {
+
+    describe('Our sute section', () => {
+
+        beforeEach('code for every test', () => {
+            // repetative code EX: login
+        })
+
+        it('some test name', () => {
+
+        })
+    })
+
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+
+    })
+
+    it('third test', () => {
+
+    })
+ 
+
+})
+
+describe('Our second suite', () => {
+
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+
+    })
+
+    it('third test', () => {
+
+    })
+ 
+
+})
