afea71b68e9fc7a38a2d43f16343ff18266ea946
Lyric Harrison <lharrison@diesellaptops.com>
2022-05-18 11:52:44 -0400

    part 15 completed

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -164,7 +164,7 @@ describe('Our sute section', () => {
     })
 
     
-    it.only('check boxes', () => {
+    it('check boxes', () => {
         //go to website
         cy.visit('/')
         cy.contains('Modal & Overlays').click()
@@ -174,6 +174,35 @@ describe('Our sute section', () => {
         cy.get('[type="checkbox"]').eq(0).click({force: true})
         cy.get('[type="checkbox"]').eq(1).check({force: true})
 
+    })
+
+    it.only('Lists and dropdowns', () => {
+        //go to website
+        cy.visit('/')
+        //#!
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        cy.get('nav nb-select').then( dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                if( index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+            })
+        })
 
     })
 
